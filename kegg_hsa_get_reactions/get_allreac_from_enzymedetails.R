756f75d78f1bf34d7991729e6e0afb72e340451f
glm729 <georgemalone@outlook.com.au>
2020-10-07 16:30:50 +0800

    kegg_hsa_get_reactions: Small improvement to function

--- kegg_hsa_get_reactions/get_allreac_from_enzymedetails.R
+++ kegg_hsa_get_reactions/get_allreac_from_enzymedetails.R
@@ -3,10 +3,7 @@
 #' @return Sorted, unique character vector of KEGG reaction IDs found.
 get_allreac_from_enzymedetails <- function(enzymedetails) {
   allreac_raw <- unlist(lapply(enzymedetails, function(x) { x$ALL_REAC }))
-  allreac_spl <- unlist(sapply(
-    allreac_raw,
-    function(x) { strsplit(x, " ")[[1]] }
-  ))
+  allreac_spl <- unlist(strsplit(allreac_raw, " "))
   clean <- sapply(
     allreac_spl,
     function(x) { gsub("[^R0-9]", '', x, perl = TRUE) }
