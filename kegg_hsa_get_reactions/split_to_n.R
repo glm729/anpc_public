30283fef9b5fb391f20009348ad9b290885d243a
glm729 <georgemalone@outlook.com.au>
2020-10-07 08:52:34 +0800

    kegg_hsa_get_reactions: Updated split_to_n.R

--- kegg_hsa_get_reactions/split_to_n.R
+++ kegg_hsa_get_reactions/split_to_n.R
@@ -1,27 +1,25 @@
-## Copyright (c) 2020. Mr. George L. Malone.
-## All rights reserved.
 #' Given a vector, split this into a list of entries of group size n.
 #' @param obj Vector to split.
 #' @param n Size to split vector by.
 #' @return List containing elements of `obj`, in groups of `n`.
 split_to_n <- function(obj, n) {
-  ## Initialise list index;
-  k <- 1
-  ## Open the cage;
-  canary <- 0
-  ## Prepare the empty list;
-  split <- vector(mode = 'list', length = ceiling(length(obj) / n))
-  for (i in seq_along(obj)) {
-    ## Add to the list;
-    split[[k]] <- append(split[[k]], obj[i])
-    ## Catch the bird;
-    canary <- canary + 1
-    ## If too many birds, move to the next cage;
-    if (canary == n) {
-      k <- k + 1
-      canary <- 0
+    ## Initialise list index and marker;
+    k <- 1
+    marker <- 0
+    ## Prepare the empty list;
+    split <- vector(mode = 'list', length = ceiling(length(obj) / n))
+    ## Loop over the object;
+    for (i in seq_along(obj)) {
+        ## Add to the list;
+        split[[k]] <- append(split[[k]], obj[i])
+        ## Iterate marker;
+        marker <- marker + 1
+        ## If marker at n, iterate k and reset marker;
+        if (marker == n) {
+            k <- k + 1
+            marker <- 0
+        }
     }
-  }
-  ## Aviary;
-  return(split)
+    ## Return the list;
+    return(split)
 }
